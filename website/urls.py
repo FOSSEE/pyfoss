b0499f0ddf78f631121e2c237e12a35081e8e953
Jayaram Pai <rush2jrp@gmail.com>
2013-11-07 12:29:09 +0530

    fixed external linking in sidebar block

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 
 	# Main pages dispatcher
-	url(r'^$', 'website.views.home', name="home"),
+	url(r'^$', 'website.views.dispatcher', name="home"),
 	url(r'^(?P<permalink>.+)/$', 'website.views.dispatcher', name="dispatcher"),
 )
--- website/views.py
+++ website/views.py
@@ -18,24 +18,26 @@ def block_sort(obj):
 	items = sorted(items, key=get_pos)
 	return items
 
-def home(request):
-	return render_to_response('home.html')
+def get_blocks():
+    sidebar = Block.objects.get(block_name = "sidebar")
+    footer = Block.objects.get(block_name = "footer")
+    blocks = {
+        'navs': Nav.objects.order_by('position'),
+        'sidebar': block_sort(sidebar),
+        'footer': block_sort(footer)
+    }
+    return blocks
 
 def dispatcher(request, permalink=''):
-	page = get_object_or_404(Page, permalink=permalink)
-	navs = Nav.objects.order_by('position')
+    if permalink == '':
+        permalink = 'home'
 
-	sidebar = Block.objects.get(block_name = "sidebar")
-	footer = Block.objects.get(block_name = "footer")
-	
-	#
-	sidebar = block_sort(sidebar)
-	footer = block_sort(footer)
-
-	context = {
-		'navs': navs,
+    page = get_object_or_404(Page, permalink=permalink)
+    blocks = get_blocks()
+    context = {
 		'page': page,
-		'sidebar': sidebar,
-		'footer': footer
+		'navs': blocks['navs'],
+		'sidebar': blocks['sidebar'],
+		'footer': blocks['footer']
 	}
-	return render_to_response('page.html', context)
+    return render_to_response('page.html', context)
