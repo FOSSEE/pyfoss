3114a68730bd781f5da97368708e8d2e17f48320
prashantsinalkar <prashantsinalkar@gmail.com>
2017-04-12 10:57:04 +0530

    added pyhton workshop stats

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response, get_object_or_404
+from website.models import FOSSEEStats
 
 from website.models import Nav, Page, Block
 
@@ -29,16 +30,40 @@ def get_blocks():
     return blocks
 
 def dispatcher(request, permalink=''):
-    if permalink == '':
-        permalink = 'home'
+	if permalink == 'python-workshops':
+		blocks = get_blocks()
+		rows = FOSSEEStats.objects.using('fossee_in').filter(foss_name='Python', type ='Workshop').order_by('-w_id')
 
-    page = get_object_or_404(Page, permalink=permalink)
-    blocks = get_blocks()
-    context = {
-		'page': page,
-		'navs': blocks['navs'],
-		'sidebar': blocks['sidebar'],
-		'footer': blocks['footer'],
-		'permalink': permalink
-	}
-    return render(request, 'website/templates/page.html', context)
+		context = {
+			'navs': blocks['navs'],
+			'sidebar': blocks['sidebar'],
+			'footer': blocks['footer'],
+			'permalink': permalink,
+			'obj' : rows,
+		}
+		#rows = FOSSEEStats.objects.using('fossee_in').all()
+		#return render(request, 'website/templates/test.html',context)
+
+	if permalink == '' or permalink == 'home' :
+		permalink = 'home'
+		page = get_object_or_404(Page, permalink=permalink)
+		blocks = get_blocks()
+		context = {
+			'page': page,
+			'navs': blocks['navs'],
+			'sidebar': blocks['sidebar'],
+			'footer': blocks['footer'],
+			'permalink': permalink
+		}
+
+	if permalink != 'home' and permalink != 'python-workshops':
+		page = get_object_or_404(Page, permalink=permalink)
+		blocks = get_blocks()
+		context = {
+			'page': page,
+			'navs': blocks['navs'],
+			'sidebar': blocks['sidebar'],
+			'footer': blocks['footer'],
+			'permalink': permalink
+		}
+	return render(request, 'website/templates/page.html', context)
