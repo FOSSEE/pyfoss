b8258c4148a885cdb9597991d72583834612f03a
prashantsinalkar <prashantsinalkar@gmail.com>
2019-02-23 16:35:05 +0530

    added template directory

--- pyfoss/settings.py
+++ pyfoss/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'pyfoss.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [PROJECT_DIR + '/static/website/templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
