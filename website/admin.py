3cb116785ffce63479db29b424bac5f75c13fdf7
prashantsinalkar <prashantsinalkar@gmail.com>
2019-02-23 14:28:10 +0530

    fixed formating

--- website/admin.py
+++ website/admin.py
@@ -4,33 +4,41 @@ from nested_inline.admin import NestedModelAdmin, NestedStackedInline, NestedTab
 
 from website.models import Nav, SubNav, Page, Link, LinkBox, TextBox, Block
 
+
 class SubNavInline(admin.TabularInline):
     model = SubNav
     extra = 0
+
 
 class NavAdmin(admin.ModelAdmin):
     list_display = ('nav_name', 'link', 'visible')
     inlines = [SubNavInline]
 
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('heading', 'permalink', 'visible')
+
 
 class LinkInline(NestedTabularInline):
     model = Link
     extra = 0
+
 
 class LinkBoxInline(NestedStackedInline):
     model = LinkBox
     inlines = [LinkInline]
     extra = 0
 
+
 class TextBoxInline(NestedStackedInline):
     model = TextBox
     extra = 0
 
+
 class BlockAdmin(NestedModelAdmin):
     model = Block
     inlines = [LinkBoxInline, TextBoxInline]
+
 
 admin.site.register(Nav, NavAdmin)
 admin.site.register(Page, PageAdmin)
