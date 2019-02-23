5c538463fb53ccd5e9d3642646a891393756e0e6
prashantsinalkar <prashantsinalkar@gmail.com>
2019-02-23 14:34:21 +0530

    fixed code

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from website.models import FOSSEEStats, TBCPYTHONBook
 
 from website.models import Nav, Page, Block
 
+
 def block_sort(obj):
     linkboxes = obj.linkbox_set.all()
     textboxes = obj.textbox_set.all()
@@ -19,9 +20,10 @@ def block_sort(obj):
     items = sorted(items, key=get_pos)
     return items
 
+
 def get_blocks():
-    sidebar = Block.objects.get(block_name = "sidebar")
-    footer = Block.objects.get(block_name = "footer")
+    sidebar = Block.objects.get(block_name="sidebar")
+    footer = Block.objects.get(block_name="footer")
     blocks = {
         'navs': Nav.objects.order_by('position'),
         'sidebar': block_sort(sidebar),
@@ -29,59 +31,40 @@ def get_blocks():
     }
     return blocks
 
+
 def dispatcher(request, permalink=''):
-    context = {}
+
+    blocks = get_blocks()
+    context = {
+        'navs': blocks['navs'],
+        'sidebar': blocks['sidebar'],
+        'footer': blocks['footer'],
+        'permalink': permalink,
+    }
 
     if permalink == 'python-workshops':
-        blocks = get_blocks()
-        rows = FOSSEEStats.objects.using('fossee_new').filter(foss_name='Python', type ='Workshop').order_by('-w_id')
-        print("----------", rows)
-        python_wokshop_page_content = Page.objects.get(permalink='python-workshops-page')
-        context = {
-            'page':python_wokshop_page_content,
-            'navs': blocks['navs'],
-            'sidebar': blocks['sidebar'],
-            'footer': blocks['footer'],
-            'permalink': permalink,
-            'obj': rows
-        }
+        rows = FOSSEEStats.objects.using('fossee_new').filter(
+            foss_name='Python', type='Workshop').order_by('-w_id')
+        python_wokshop_page_content = Page.objects.get(
+            permalink='python-workshops-page')
+        context['page'] = python_wokshop_page_content
+        context['obj'] = rows
 
     if permalink == 'textbook-companions-for-academics':
-        blocks = get_blocks()
-        textbook_companions_for_academics = Page.objects.get(permalink='textbook-companions-for-academics-page')
-        completed_books = TBCPYTHONBook.objects.using('tbcpython').values('id', 'title', 'author').filter(approved=True).order_by('id')
+        textbook_companions_for_academics = Page.objects.get(
+            permalink='textbook-companions-for-academics')
+        completed_books = TBCPYTHONBook.objects.using('tbcpython').values(
+            'id', 'title', 'author').filter(approved=True).order_by('id')
+        context['page'] = textbook_companions_for_academics
+        context['obj'] = completed_books
 
-        context = {
-            'page':textbook_companions_for_academics,
-            'navs': blocks['navs'],
-            'sidebar': blocks['sidebar'],
-            'footer': blocks['footer'],
-            'permalink': permalink,
-            'obj': completed_books
-        }
-
-    if permalink == '' or permalink == 'home' :
+    if permalink == '' or permalink == 'home':
         permalink = 'home'
         page = get_object_or_404(Page, permalink=permalink)
-        blocks = get_blocks()
-        context = {
-            'page': page,
-            'navs': blocks['navs'],
-            'sidebar': blocks['sidebar'],
-            'footer': blocks['footer'],
-            'permalink': permalink
-        }
-        context['permalink'] = permalink
+        context['page'] = page
 
     if permalink != 'home' and permalink != 'python-workshops' and permalink != 'textbook-companions-for-academics':
         page = get_object_or_404(Page, permalink=permalink)
-        blocks = get_blocks()
-        context = {
-            'page': page,
-            'navs': blocks['navs'],
-            'sidebar': blocks['sidebar'],
-            'footer': blocks['footer'],
-            'permalink': permalink
-        }
-    return render(request, "website/page.html", context)
+        context['page'] = page
 
+    return render(request, "website/page.html", context)
