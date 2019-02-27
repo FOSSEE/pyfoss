1b2b32a9ec1f52128f10cea500435ebf5b3bf806
prashantsinalkar <prashantsinalkar@gmail.com>
2019-02-27 18:49:01 +0530

    removed unused model and added unique attribute for page link

--- website/models.py
+++ website/models.py
@@ -26,7 +26,7 @@ class SubNav(models.Model):
 
 class Page(models.Model):
     """Page that will be dispatched"""
-    permalink = models.CharField(max_length=200)
+    permalink = models.CharField(max_length=200, unique=True)
     heading = models.CharField(max_length=500)
     content = models.TextField()
     visible = models.BooleanField()
@@ -127,22 +127,3 @@ class FOSSEEStats(models.Model):
 
     class Meta:
         db_table = 'workshop'
-
-
-class TBCPYTHONBook(models.Model):
-    title = models.CharField(max_length=500)
-    author = models.CharField(max_length=300)
-    category = models.CharField(max_length=32, choices=CATEGORY)
-    publisher_place = models.CharField(max_length=150)
-    isbn = models.CharField(max_length=50)
-    edition = models.CharField(max_length=15)
-    year_of_pub = models.CharField(max_length=4)
-    no_chapters = models.IntegerField(default=0, blank=True)
-    contributor = models.IntegerField(default=0, blank=True)
-    reviewer = models.IntegerField(default=0, blank=True)
-    approved = models.BooleanField(default=False)
-    start_time = models.DateField(null=True, default=None)
-    end_time = models.DateField(null=True, default=None)
-
-    class Meta:
-        db_table = 'tbc_book'
